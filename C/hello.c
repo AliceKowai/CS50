4191161c9548c924ecd7b385a563845550ff2778
Alice Costa <devalice117@gmail.com>
2023-05-25 18:44:53 -0300

    Fazendo operações simples

--- C/hello.c
+++ C/hello.c
@@ -2,9 +2,16 @@
 
 int main(void) {
     char nome[100];
+    int idade;
+    int idadeDele;
 
     printf("Seu nome? \n");
     scanf("%s", nome);
-    printf("Hello, %s" , nome);
+    printf("Qual e a sua idade? \n");
+    scanf("%i", &idade);
+    printf("Qual e a idade dele? \n");
+    scanf("%i", &idadeDele);
+    int diferenca = idade - idadeDele;
+    printf("Oi, %s. A diferenca entre as idades de voces e %i\n", nome, diferenca);  
     return 0;
 }
